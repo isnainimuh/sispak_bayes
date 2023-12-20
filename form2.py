f96408c78a975432afb4bcc2b36cecd635b1b1fd
Isnaini M <87063578+isnainimuh@users.noreply.github.com>
2023-12-21 06:31:32 +0700

    Delete form2.py
    File sudah tidak dibutuhkan, di replace oleh forms.py

--- form2.py
+++
@@ -1,9 +0,0 @@
-from wtforms import Form, StringField, validators, SubmitField
-
-class NewPenyakitForm(Form):
-    nama_penyakit = StringField('Nama Penyakit', [validators.DataRequired()])
-    submit = SubmitField("Submit")
-
-class NewGejalaForm(Form):
-    gejala = StringField('Gejala',[validators.DataRequired()])
-    #submit = SubmitField("Submit")
